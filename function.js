6cbf4b390b03402362731031ddd2c94a41af7281
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-07 22:00:21 +0200

    Function practice

--- function.js
+++ function.js
@@ -3,3 +3,12 @@ function helloWorld() {
 }
 
 helloWorld(); // Call the function to execute it
+
+function addNumbers(a, b) {
+  // let a = 5;
+  // let b = 10;
+  let sum = a + b;
+  console.log("The sum is: " + sum);
+}
+
+addNumbers(10, 20); // Call the function to execute it
